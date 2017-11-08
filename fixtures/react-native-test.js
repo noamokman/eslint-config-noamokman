dbf7bbf8007721caf9431b4f85d72d5e67bd5195
Noam Okman <noamokman@gmail.com>
2017-11-08 15:36:13 +0200

    feat(import): added import plugin

--- fixtures/react-native-test.js
+++ fixtures/react-native-test.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React from 'react'; // eslint-disable-line import/no-extraneous-dependencies
+import {Text} from 'react-native'; // eslint-disable-line import/no-extraneous-dependencies
 
 describe('test', function () {
   this.timeout(5000);
--- fixtures/react-native.js
+++ fixtures/react-native.js
@@ -1,4 +1,4 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React from 'react'; // eslint-disable-line import/no-extraneous-dependencies
+import {Text} from 'react-native'; // eslint-disable-line import/no-extraneous-dependencies
 
 export default () => <Text>{'Hello'}</Text>;
